df2240d4c40e2bf99c0a1b4147c9e6fb609dc338
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-11-02 18:43:40 +0200

    add database and other components

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Customers = () => {
+  return (
+    <div>
+      <p>Customers Page</p>
+    </div>
+  )
+}
+
+export default Customers
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Invoices = () => {
+  return (
+    <div>
+      <p>Invoices Page</p>
+    </div>
+  )
+}
+
+export default Invoices
